3ce7df5765221cc624c7cdabe19a074dee9eba1e
Pranav R Prasad <pranavrp.pr@gmail.com>
2021-10-07 20:00:00 +0530

    Update PushPop.java

--- PushPop.java
+++ PushPop.java
@@ -1,50 +1,55 @@
 import java.util.*;  
+
 public class StackPushPopExample   
 {  
-public static void main(String args[])   
-{  
-//creating an object of stack
-Stack <Integer> stk = new Stack<>();  
-System.out.print("stack: " + stk);  
-//pushing elements into the stack  
-pushelmnt(stk, 20);  
-pushelmnt(stk, 13);  
-pushelmnt(stk, 89);  
-pushelmnt(stk, 90);  
-pushelmnt(stk, 11);  
-pushelmnt(stk, 45);  
-pushelmnt(stk, 18);  
-
-popelmnt(stk);  
-popelmnt(stk);  
- 
-try   
-{  
-popelmnt(stk);  
-}   
-catch (EmptyStackException e)   
-{  
-System.out.print("empty stack");  
-}  
+ public static void main(String args[])   
+ {  
+ //creating an object of stack
+ Stack <Integer> stk = new Stack<>();
+  
+ System.out.print("stack: " + stk);
+  
+ //pushing elements into the stack  
+ pushelmnt(stk, 20);  
+ pushelmnt(stk, 13);  
+ pushelmnt(stk, 89);  
+ pushelmnt(stk, 90);  
+ pushelmnt(stk, 11);  
+ pushelmnt(stk, 45);  
+ pushelmnt(stk, 18);  
+ popelmnt(stk);  
+ popelmnt(stk); 
+  
+ try   
+ {  
+  popelmnt(stk);  
+ }   
+ catch (EmptyStackException e)   
+ {  
+  System.out.print("empty stack");  
+ }  
 }  
 
 static void pushelmnt(Stack stk, int x)   
 {  
-//invoking push()     
-stk.push(new Integer(x));  
-System.out.print("push -> " + x);  
-//prints modified stack  
-System.out.println("stack: " + stk);  
-}  
-//performing pop operation  
-static void popelmnt(Stack stk)   
-{  
-System.out.print("pop -> ");  
-//invoking pop() method   
-Integer x = (Integer) stk.pop();  
-System.out.print(x);   
-System.out.println("stack: " + stk);  
-}  
+ //invoking push()     
+ stk.push(new Integer(x));  
+ System.out.print("push -> " + x);
+ 
+ //prints modified stack  
+ System.out.println("stack: " + stk);  
+ }
+ 
+ //performing pop operation  
+ static void popelmnt(Stack stk)   
+ {  
+  System.out.print("pop -> ");  
+ //invoking pop() method   
+ Integer x = (Integer) stk.pop();  
+ System.out.print(x);   
+ System.out.println("stack: " + stk);  
+ }  
 }
+
 //end
 
